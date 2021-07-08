cecf547a0451345312488b4a82bc822130de91fc
camila martinez <martinezce_@hotmail.com>
2021-07-08 09:44:25 +0300

    modificamos la ruta de redireccion

--- src/Service/ProductNormalize.php
+++ src/Service/ProductNormalize.php
@@ -6,7 +6,8 @@ use App\Entity\ImageProduct;
 use App\Entity\Product;
 use Symfony\Component\HttpFoundation\UrlHelper;
 
-class ProductNormalize {
+class ProductNormalize
+{
     private $urlHelper;
 
     public function __construct(UrlHelper $constructorDeURL)
@@ -16,32 +17,41 @@ class ProductNormalize {
 
     /**
      * Normalize an product.
-     * 
-     * @param Product 
-     * 
+     *
+     * @param Product
+     *
      * @return array|null
      */
-    public function productNormalize (Product $product): ?array {
-
+    public function productNormalize(Product $product): ?array
+    {
         $img_principal = '';
-        if($product->getImgPrincipal()) {
-            $img_principal = $this->urlHelper->getAbsoluteUrl('/product/img_principal/'.$product->getImgPrincipal());
+        if ($product->getImgPrincipal()) {
+            $img_principal = $this->urlHelper->getAbsoluteUrl('/media/img/product/'.$product->getImgPrincipal());
+        }
+
+        $productsImage = [];
+        foreach ($product->getImageProducts() as $productImage) {
+            $productsImage[] = $this->urlHelper->getAbsoluteUrl('/media/img/product/'.$productImage->getImgFile());
         }
 
         return [
+            'id' => $product->getId(),
             'name' => $product->getName(),
             'category_name' => $product->getCategory()->getName(),
             'category_id' => $product->getCategory()->getId(),
             'slug' => $product->getSlug(),
-            'img_principal' => $product->getImgPrincipal(),
+            'img_principal' => $img_principal,
             'weight' => $product->getWeight(),
             'price' => $product->getPrice(),
-            'user_name' => $product->getUser()->getName(),
-            'user_company' => $product->getUser()->getCompanyName(),
-            'user_phone' => $product->getUser()->getPhoneNumber(),
-            'user_city' => $product->getUser()->getCity(),
-            'user_address' => $product->getUser()->getAddress(),
-            'user_avatar' => $product->getUser()->getAvatar(),
+            'imageProduct' => $productsImage,
+            'user_admin' => [
+                'user_name' => $product->getUser()->getName(),
+                'user_company' => $product->getUser()->getCompanyName(),
+                'user_phone' => $product->getUser()->getPhoneNumber(),
+                'user_city' => $product->getUser()->getCity(),
+                'user_address' => $product->getUser()->getAddress(),
+                'user_avatar' => $product->getUser()->getAvatar(),
+            ],
         ];
     }
 }
