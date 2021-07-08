f1d864f5beba548f0cf2f0a84d1857f1a1b629cf
camila martinez <martinezce_@hotmail.com>
2021-07-08 09:59:04 +0300

    feat(admin/ApiProductController): Creamos add

--- src/Controller/admin/ApiProductController.php
+++ src/Controller/admin/ApiProductController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\admin;
 
 use App\Entity\Product;
+use App\Repository\CategoryRepository;
 use App\Repository\UserRepository;
 use App\Repository\ProductRepository;
 use App\Service\ProductNormalize;
@@ -16,7 +17,7 @@ use Symfony\Component\String\Slugger\SluggerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 /**
- * @Route("/api/product", name="api_product_")
+ * @Route("/api/admin/product", name="api_product_admin")
  */
 class ApiProductController extends AbstractController
 {
@@ -27,34 +28,18 @@ class ApiProductController extends AbstractController
      *      methods={"GET"}
      * )
      */
-    public function index(Request $request, ProductRepository $productRepository, ProductNormalize $productNormalize): Response
-    {
-
+    public function index(
+        Request $request,
+        ProductRepository $productRepository,
+        ProductNormalize $productNormalize
+    ): Response {
         /* USAR PARA EL CONTROLADOR PRODUCT DE ADMIN*/
-        $user = $this->getUser(); 
+        $user = $this->getUser();
 
-         if ($user->$this->isGranted('ROLE_ADMIN')) {
+        if ($user->$this->isGranted('ROLE_ADMIN')) {
             return $productRepository->findBy(['user' => $user]);
-        }else{
+        } else {
             return $productRepository->findAll();
-        } 
-        // Si tengo usuario $this->isGranted('ROLE_ADMIN')
-            // Devuelvo solo los productos del adm
-            // $productRepository->findBy(['user' => $user]);
-        // Si no
-            // Devuelvo todos los productos
-            // $productRepository->findAll();
-
-        if ($request->query->has('termino')) {
-            $productEntities = $productRepository->findByTerm($request->query->get('termino'));
-
-            $data = [];
-
-            foreach ($productEntities as $theProductEntity) {
-                $data[] = $productNormalize->productNormalize($theProductEntity);
-            }
-    
-            return $this->json($data);
         }
 
         $productEntities = $productRepository->findAll();
@@ -67,4 +52,138 @@ class ApiProductController extends AbstractController
 
         return $this->json($data);
     }
+
+    /**
+     * @Route(
+     *      "",
+     *      name="post",
+     *      methods={"POST"}
+     * )
+     */
+    public function add(
+        Request $request,
+        EntityManagerInterface $entityManager,
+        ValidatorInterface $validator,
+        CategoryRepository $categoryRepository,
+        ProductNormalize $productNormalize,
+        UserRepository $userRepository,
+    
+        SluggerInterface $slug
+    ): Response {
+
+        /* falta subida de imagenes de producto */
+        /* falta recuperar entidad de usuario segun quien este logueado */
+        $data = $request->request;
+
+        dump($data);
+        dump($request->files);
+
+        /* $theUserEntity = $this->getUser(); */
+        $theCategoryEntity = $categoryRepository->find($data->get('category_id'));
+        $theUserEntity = $userRepository->find(4);
+        $slugProduct = $slug->slug($data->get('name'));
+
+        $theProductEntity = new Product();
+
+        $theProductEntity->setName($data->get('name'));
+        $theProductEntity->setCategory($theCategoryEntity);
+        $theProductEntity->setSlug($slugProduct);
+       /*  $theProductEntity->setImgPrincipal($data->get('img_principal')); */
+        $theProductEntity->setWeight($data->get('weight'));
+        $theProductEntity->setPrice($data->get('price'));
+        /* $theProductEntity->setImageProduct($data->get('image_product')); */
+        /* $theProductEntity->setUser($theUserEntity); */
+        $theProductEntity->setUser($theUserEntity);
+
+        if ($request->files->has('img_principal')) {
+            $imgPrinciplaFile= $request->files->get('img_principal');
+            
+           $imgOriginalFilename = pathinfo($imgPrinciplaFile->getClientOriginalName(), PATHINFO_FILENAME);
+            dump($imgOriginalFilename);
+
+           
+            $imgNewFilename = $slugProduct.'-'.uniqid().'.'.$imgPrinciplaFile->guessExtension();
+            dump($imgNewFilename);
+
+            try {
+                $imgPrinciplaFile->move(
+                    $request->server->get('DOCUMENT_ROOT') . DIRECTORY_SEPARATOR . 'media/img/product',
+                    $imgNewFilename
+                );
+            } catch (FileException $e) {
+                throw new \Exception($e->getMessage());
+            }
+
+            $theProductEntity->setImgPrincipal($imgNewFilename);
+        }
+
+        $errors = $validator->validate($theProductEntity);
+        
+        if (count($errors) > 0) {
+            $dataErrors = [];
+
+            /** @var \Symfony\Component\Validator\ConstraintViolation $error */
+            foreach ($errors as $error) {
+                $dataErrors[] = $error->getMessage();
+            }
+
+            return $this->json([
+                'status' => 'error',
+                'data' => [
+                    'errors' => $dataErrors
+                    ]
+                ],
+                Response::HTTP_BAD_REQUEST);
+        } 
+
+        $entityManager->persist($theProductEntity);
+
+        // $theProductentity no tiene id.
+
+        $entityManager->flush();
+
+        return $this->json(
+            $productNormalize->productNormalize($theProductEntity),
+            Response::HTTP_CREATED
+        );
+    }
+
+    /**
+     * @Route(
+     *      "/{id}",
+     *      name="put",
+     *      methods={"PUT"},
+     *      requirements={
+     *          "id": "\d+"
+     *      }
+     * )
+     */
+    public function update(
+        int $id,
+        EntityManagerInterface $entityManager,
+        ProductRepository $productRepository,
+        Request $request
+    ): Response {
+        $theProductEntity = $productRepository->find($id);
+
+        if (!$theProductEntity) {
+            return $this->json([
+                'message' => sprintf('No he encontrado el producto con id.: %s', $id)
+            ], Response::HTTP_NOT_FOUND);
+        }
+        $data = $request->request;
+
+        $theProductEntity->setName($data->get('name'));
+        $theProductEntity->setCategory($data->get('category'));
+        $theProductEntity->setImgPrincipal($data->get('img_principal'));
+        $theProductEntity->setWeight($data->get('weight'));
+        $theProductEntity->setPrice($data->get('price'));
+        $theProductEntity->setSlug($data->get('slug'));
+
+        $entityManager->flush();
+
+        return $this->json(null, Response::HTTP_NO_CONTENT);
+    }
+
 }
+
