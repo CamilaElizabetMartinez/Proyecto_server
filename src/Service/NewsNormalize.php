0421ddc4c35b70e1bf27f56a670638cfdd127aa8
camila martinez <martinezce_@hotmail.com>
2021-11-25 11:21:42 +0200

    feat(src/service): Creacion de newNormalize

---
+++ src/Service/NewsNormalize.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\ImageNews;
+use App\Entity\News;
+use Symfony\Component\HttpFoundation\UrlHelper;
+
+class NewsNormalize
+{
+    private $urlHelper;
+
+    public function __construct(UrlHelper $constructorDeUrl)
+    {
+        $this->urlHelper = $constructorDeUrl;
+    }
+
+    /**
+     * Normalize an News.
+     *
+     * @param News
+     *
+     * @return array|null
+     */
+    public function newsNormalize(News $news, ImageNews $imageNews): ?array
+    {
+        $img_file = '';
+        if ($imageNews->getImgFile()) {
+            $img_file = $this->urlHelper->getAbsoluteUrl('/media/img/news/'.$imageNews->getImgFile());
+        }
+
+        return [
+            'id' => $news->getId(),
+            'title' => $news->getTitle(),
+            'slug' => $news->getslug(),
+            'subtitile' => $news->getSubtitle(),
+            'description' => $news->getDescription(),
+            'img_file' => $img_file,
+        ];
+    }
+
+};
