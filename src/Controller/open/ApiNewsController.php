1fcbabbebe834b483e1b3a504a80c48fd7fc6b57
camila martinez <martinezce_@hotmail.com>
2021-11-25 11:18:36 +0200

    feat(src/controller/open): Creacion de controlador ApiNews

---
+++ src/Controller/open/ApiNewsController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller\open;
+
+use App\Entity\News;
+use App\Repository\NewsRepository;
+use App\Service\NewsNormalize;
+use App\Entity\User;
+use App\Repository\UserRepository;
+use App\Service\UserNormalize;
+use App\Controller\ValidatorInterface;
+use App\Controller\UserPasswordHasherInterface;
+use App\Entity\ImageNews;
+use App\Repository\ImageNewsRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Lcobucci\JWT\Validation\Validator;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\FrameworkBundle\HttpCache\HttpCache;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/api/news", name="api_news")
+ */
+class ApiNewsController extends AbstractController
+{
+   
+
+};
