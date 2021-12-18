255b5c2d0e6f5c6560df248819caa58a077459b8
camila martinez <martinezce_@hotmail.com>
2021-12-18 17:43:58 +0200

    feat(src/Controller/superAdmin): Creacion de endpoint Index para mostrar noticias

--- src/Controller/superAdmin/ApiNewsController.php
+++ src/Controller/superAdmin/ApiNewsController.php
@@ -2,10 +2,27 @@
 
 namespace App\Controller\superAdmin;
 
+use App\Repository\NewsRepository;
+use App\Service\NewsNormalize;
 /**
  * @Route("/api/super_admin/news", name="api_news_super_admin")
  */
 
 class ApiNewsController extends AbstractController
 {
+    /**
+    * @Route(
+    *      "",
+    *      name="cget",
+    *      methods={"GET"}
+    * )
+    * @IsGranted("ROLE_SUPER_ADMIN")
+    */
+    
+    public function Index(
+        NewsRepository $newsRepository,
+        NewsNormalize $newsNormalize
+    ): Response {
+    }
+
 }
