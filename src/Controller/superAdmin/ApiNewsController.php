12104ce6355f51333d428e968908d273895e27e1
camila martinez <martinezce_@hotmail.com>
2021-12-18 17:37:01 +0200

    feat(src/Controller/superAdmin): Creacion de ApiNewsController

---
+++ src/Controller/superAdmin/ApiNewsController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controller\superAdmin;
+
+/**
+ * @Route("/api/super_admin/news", name="api_news_super_admin")
+ */
+
+class ApiNewsController extends AbstractController
+{
+}
