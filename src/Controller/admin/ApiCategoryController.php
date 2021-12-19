a287d5240900b651aa19e444a802d6beb1c34955
camila martinez <martinezce_@hotmail.com>
2021-12-19 17:28:56 +0200

    feat(src/Controller/admin): Creacion de enpoint index para mostrar las categorias

--- src/Controller/admin/ApiCategoryController.php
+++ src/Controller/admin/ApiCategoryController.php
@@ -13,4 +13,33 @@ use Symfony\Component\Routing\Annotation\Route;
 */
 class ApiCategoryController extends AbstractController
 {
+    /**
+    * @Route(
+    *      "",
+    *      name="cget",
+    *      methods={"GET"}
+    * )
+    */
+    public function index(
+        CategoryRepository $categoryRepository,
+        CategoryNormalize $categoryNormalize
+    ): Response {
+
+        //Recupero todas las categorias
+        $categoryEntities = $categoryRepository->findAll();
+        
+        //Declaro un array vacio para guardar los datos normalizados
+        $categoriesNormalized = [];
+
+        //Se normaliza cada entidad de categoria y se guarda en un array vacio
+        foreach ($categoryEntities as $theCategoryEntity) {
+            $categoriesNormalized[] = $categoryNormalize->CategoryNormalize($theCategoryEntity);
+        }
+
+        //retorno en formato JSON las categorias normalizadas
+        return $this->json(
+            $categoriesNormalized,
+            Response::HTTP_OK
+        );
+    }
 }
