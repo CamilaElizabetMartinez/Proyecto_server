2ac09b96cfa6fb655ebe2467a1386a3768c42822
camila martinez <martinezce_@hotmail.com>
2021-12-19 17:08:52 +0200

    feat(src/Controller/superAdmin): Creacion de enpoint uploanNewsImage para subir la imagen principal y las  imagenes de noticias

--- src/Controller/superAdmin/ApiNewsController.php
+++ src/Controller/superAdmin/ApiNewsController.php
@@ -2,11 +2,17 @@
 
 namespace App\Controller\superAdmin;
 
+use App\Entity\ImageNews;
 use App\Entity\News;
 use App\Repository\NewsRepository;
 use App\Service\NewsNormalize;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\String\Slugger\SluggerInterface;
 
 /**
@@ -174,7 +180,102 @@ class ApiNewsController extends AbstractController
             Response::HTTP_NO_CONTENT
         );
     }
-    }
-    }
 
+    /**
+    * @Route(
+    *      "/uploadNewsImage/{id}",
+    *      name="uploadNewsImage",
+    *      methods={"POST"},
+    *      requirements={
+    *          "id": "\d+"
+    *      }
+    * )
+    * @IsGranted("ROLE_SUPER_ADMIN")
+    */
+    public function uploadNewsImage(
+        News $newsEntity,
+        Request $request,
+        EntityManagerInterface $entityManager,
+        SluggerInterface $slug
+    ): Response {
+        //Si el archivo que llega de la solicitud posee img_principal
+        //Lo obtiene y guarda en una variable
+        if ($request->files->has('img_principal')) {
+            $imageFile = $request->files->get('img_principal');
+
+            //Se genera un unico ID,recupera la extencion,la concatena
+            //Y guarda en una variable
+            $newFilename = uniqid().'.'.$imageFile->guessExtension();
+
+            //Intenta mover el archivo y necesita 2 parametros
+            try {
+                $imageFile->move(
+                    //1º La ruta donde se va a mover
+                    $request->server->get('DOCUMENT_ROOT') . DIRECTORY_SEPARATOR . 'media/img/news',
+                    //2º El nuevo nombre del archivo
+                    $newFilename
+                );
+                // Si sucede algo inesperado captura el error
+            } catch (FileException $e) {
+                // Y lanza la excepción
+                throw new \Exception($e->getMessage());
+            }
+            
+            //Setea la imagen principal por el nuevo nombre del archivo
+            $newsEntity->setImgPrincipal($newFilename);
+            
+            //Prepara y ejecuta la sentencia
+            $entityManager->persist($newsEntity);
+            $entityManager->flush();
+        }
+
+        //Si el archivo que llega de la solicitud posee img_file
+        //Lo obtiene y guarda en una variable
+        if ($request->files->has('img_file')) {
+            $imageNewsArray = $request->files->get('img_file');
+
+            //Recorre el array de imagen de noticias
+            foreach ($imageNewsArray as $theImageNews) {
+                //Recupera el nombre original de la imagen
+                $imgOriginalFilename = pathinfo($theImageNews->getClientOriginalName(), PATHINFO_FILENAME);
+
+                //Genera un Slug
+                $slugImagePrincipal = $slug->slug($imgOriginalFilename);
+
+                //Al slug se concatena el unido ID generado + la extension
+                $imgNewFilename = $slugImagePrincipal.'-'.uniqid().'.'. $theImageNews->guessExtension();
+
+                //Intenta mover el archivo y necesita 2 parametros
+                try {
+                    $theImageNews->move(
+                        //1º La ruta donde se va a mover
+                        $request->server->get('DOCUMENT_ROOT') . DIRECTORY_SEPARATOR . 'media/img/product',
+                        //2º El nuevo nombre del archivo
+                        $imgNewFilename
+                    );
+                    // Si sucede algo inesperado captura el error
+                } catch (FileException $e) {
+                    // Y lanza la excepción
+                    throw new \Exception($e->getMessage());
+                }
+
+                //Se crea una instancia
+                $theImageNewsEntity = new ImageNews();
+
+                //Setea la imagen de la noticia por el nuevo nombre del archivo
+                $theImageNewsEntity->setImgFile($imgNewFilename);
+
+                //Setea la noticia
+                $theImageNewsEntity->setNews($newsEntity);
+            
+                //Se envia a la BBDD
+                $entityManager->flush();
+            }//End foreach
+
+            //Retorna en formato JSON
+            return $this->json(
+                Response::HTTP_CREATED
+            );
+        }
+    }
 }
