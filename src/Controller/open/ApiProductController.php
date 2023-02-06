49c0725120fce145853d2efa6891b071875ebcac
camila martinez <martinezce_@hotmail.com>
2023-02-06 19:01:03 +0200

    refactor(src/controller/open/ApiProductController.php): Creacion de controlador

--- src/Controller/open/ApiProductController.php
+++ src/Controller/open/ApiProductController.php
@@ -2,24 +2,12 @@
 
 namespace App\Controller\open;
 
-use App\Entity\Product;
 use App\Repository\ProductRepository;
 use App\Service\ProductNormalize;
-use App\Entity\User;
-use App\Repository\UserRepository;
-use App\Service\UserNormalize;
-use App\Controller\ValidatorInterface;
-use App\Controller\UserPasswordHasherInterface;
-use App\Entity\Category;
-use App\Repository\CategoryRepository;
-use Doctrine\ORM\EntityManagerInterface;
-use Error;
-use Lcobucci\JWT\Validation\Validator;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
 
 /**
  * @Route("/api/product", name="api_product")
@@ -28,7 +16,6 @@ class ApiProductController extends AbstractController
 {
     /**
      * @Route(
-     *      "",
      *      name="cget",
      *      methods={"GET"}
      * )
@@ -137,63 +124,4 @@ class ApiProductController extends AbstractController
 
         return $this->json($theProductEntityNormalize);
     }
-
-   ## /**
-   ##  * @Route(
-   ##  *      "/register",
-    ## *      name="post",
-    ## *      methods={"POST"}
-    ## * )
-    ## */
-    ##public function add(
-    ##   UserNormalize $userNormalize,
-    ##    Request $request,
-    ##    EntityManagerInterface $entityManager,
-    ##    ValidatorInterface $validator,     
-    ##    UserPasswordHasherInterface $hasher
-    ##    ): Response {
-    ##    $data = json_decode($request->getContent());
-    ##             
-    ##    $user = new User();
-
-    ##    $user->setName($data->name);
-    ##    $user->setSurname1($data->surname1);
-    ##    $user->setCity($data->city);
-    ##    $user->setAddress($data->address);
-    ##    $user->setPhoneNumber($data->phoneNumber);
-    ##    $user->setEmail($data->email);
-
-    ##    $hash = $hasher->hashPassword($user, $data->password);
-    ##    $user->setPassword($hash);                        
-
-    ##    $errors = $validator->validate($user);
-
-    ##    if(count($errors) > 0) {
-    ##        $dataErrors = [];
-    ##        /** @var \Symfony\Component\Validator\ConstraintViolation $error */
-    ##        foreach($errors as $error) {
-    ##            $dataErrors[] = $error->getMessage();
-    ##        }
-    ##        
-    ##        return $this->json([
-    ##            'status' => 'error',
-    ##            'data' => [
-    ##                'errors' => $dataErrors
-    ##            ],
-    ##        ],
-    ##        Response::HTTP_BAD_REQUEST);
-    ##    }
-
-    ##    $entityManager->persist($user);
-    ##    
-    ##    $entityManager->flush();
-
-    ##   
-    ##    return $this->json(
-    ##        $userNormalize->UserNormalize($user),
-    ##        Response::HTTP_CREATED,        
-
-    ##    );
-    ##
-    ##}
 }
